c20318286b3a2295651a19157eaaf4132737e667
alymarquez <alyym.55@gmail.com>
2025-07-04 19:25:42 -0300

    Nuevas dependencias y cambios

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
-
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 
 app.use('/productos', require('./src/routes/productosRoutes'));
+
+app.use('/usuarios', require('./src/routes/usuarioRoutes'));
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`)
