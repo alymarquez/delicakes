719193e349ddb31b581e01c6131151420609ddc0
alymarquez <alyym.55@gmail.com>
2025-07-10 01:26:02 -0300

    Componente footer y css basico

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2025 Delicakes - Hecho con 💗</p>
+      <div className="redes">
+        <a href="#">Instagram</a> | <a href="#">WhatsApp</a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
