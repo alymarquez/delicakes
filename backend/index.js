cd9bd43e33b49b50f683e83c9f7aa1760d1923a7
alymarquez <alyym.55@gmail.com>
2025-07-14 11:33:56 -0300

    Cors backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
+const cors = require('cors')
 
 app.use(express.json())
+
+app.use(cors())
 
 app.use('/productos', require('./src/routes/productosRoutes'));
 
