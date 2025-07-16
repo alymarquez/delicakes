1b4636948a81e82f635395adad2450ba75eb7a65
alymarquez <alyym.55@gmail.com>
2025-07-16 14:28:02 -0300

    Servicio API categoria y cambios en roles de ruta

---
+++ frontend/src/services/categoriasApi.js
@@ -0,0 +1,21 @@
+import { apiFetch } from "./apiFetch"
+
+export const crearCategoria = async (categoriaData, token) => {
+    return apiFetch(`categorias`, 'POST', categoriaData, { 'Authorization': `Bearer ${token}`})
+}
+
+export const obtenerCategorias = async () => {
+    return apiFetch(`categorias`, 'GET')
+}
+
+export const obtenerCategoria = async (id) => {
+    return apiFetch(`categorias${id}`, 'GET')
+}
+
+export const actualizarCategoria = async (id, categoriaData, token) => {
+    return apiFetch(`categorias${id}`, 'PUT', categoriaData, { 'Authorization': `Bearer ${token}`})
+}
+
+export const eliminarCategoria = async (id, token) => {
+    return apiFetch(`categorias${id}`, 'DELETE', { 'Authorization': `Bearer ${token}`})
+}
