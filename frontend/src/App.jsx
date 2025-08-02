88d5b0dacc8d55c68ceeaabbedf967e95235dc95
alymarquez <alyym.55@gmail.com>
2025-08-02 16:11:49 -0300

    App envuelta con Authprovider, cambios en index

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthProvider'
 
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
@@ -7,21 +8,28 @@ import Home from './pages/Home';
 import Catalogo from './pages/Catalogo';
 import Nosotros from './pages/SobreNosotros';
 import Contacto from './pages/Contacto';
+
 import CardProducto from './components/CardProducto/CardProducto';
+import Login from './components/Login/Login'
+
+import './App.css'
+import Carrito from './pages/Carrito';
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/catalogo" element={<Catalogo />} />
-        <Route path="/catalogo/:categoriaId" element={<CardProducto />} />
-        <Route path="/nosotros" element={<Nosotros />} />
-        <Route path="/contacto" element={<Contacto />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalogo" element={<Catalogo />} />
+          <Route path="/catalogo/:categoriaId" element={<CardProducto />} />
+          <Route path="/nosotros" element={<Nosotros />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/carrito" element={<Carrito/>} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       <Footer />
-    </>
+    </AuthProvider>
 
   )
 }
