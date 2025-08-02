52ee92b0feac888b3f1811bd1972e458ab413e79
alymarquez <alyym.55@gmail.com>
2025-08-02 16:06:44 -0300

    Ajustes en middleware para token, JWT secret

--- backend/src/middlewares/validarJWT.js
+++ backend/src/middlewares/validarJWT.js
@@ -3,19 +3,13 @@ const jwt = require('jsonwebtoken');
 const validarJWT = (req, res, next) => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
   
-  console.log('--- Validando JWT ---');
-  console.log('Token recibido:', token);
-
   if (!token) {
     console.log('Error: Token no proporcionado.');
     return res.status(401).json({ msg: 'No token, autorización denegada' });
   }
 
   try {
-    console.log('Clave usada para verificar:', process.env.JWT_SECRET);
-
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log('Token verificado con éxito. Usuario:', decoded);
     req.usuario = decoded;
     next();
   } catch (error) {
